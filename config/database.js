14039026014369254f597e5eed8e52c6ca3770cb
Kevin-Yahir-MN <kevinyahirmn@outlook.com>
2025-04-22 16:33:57 -0700

    database

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false // Cambia a true para debugging
+});
+
+module.exports = sequelize;
