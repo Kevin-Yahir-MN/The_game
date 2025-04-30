c2e96dd14503489fa0663f1191c576cc20819f71
Kevin-Yahir-MN <kevinyahirmn@outlook.com>
2025-04-30 14:00:07 -0700

    Update game.js

--- client/game.js
+++ client/game.js
@@ -394,7 +394,7 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(() => {
                 historyIconsAnimation.isAnimating = false;
             }, 1000);
-        }, 5000);
+        }, 20000);
     }
 
     function showNotification(message, isError = false) {
